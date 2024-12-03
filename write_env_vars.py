3985bb608ab455e6caa4a2423d3554f89a36fbe1
Roshla_P <roshla_p@yahoo.com>
2024-12-03 22:32:35 +0530

    BUILD_NUMBER

--- write_env_vars.py
+++ write_env_vars.py
@@ -9,7 +9,7 @@ env_vars = os.environ
 output_file = 'jenkins_env_variables_git.txt'
 
 # Define the inputs for the testme class
-buildno = env_vars.get("BUILD_NO", "default_buildno")  # Replace with actual key from Jenkins or a default
+buildno = env_vars.get("BUILD_NUMBER", "default_buildno")  # Replace with actual key from Jenkins or a default
 MY_GLOBAL_KEY = env_vars.get("MY_GLOBAL_KEY", "default_key")  # Replace with actual key from Jenkins or a default
 
 # Create an instance of the testme class
